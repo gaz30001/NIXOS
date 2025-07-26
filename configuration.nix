6f3b520d0b72e196a2d180ea756d5b5ed0b8a731
gaz30001 <51251202+gaz30001@users.noreply.github.com>
2025-07-26 18:40:26 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,67 +1,44 @@
+{ config, pkgs, ... }:
+
 {
   imports = [ ./hardware-configuration.nix ];
 
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  boot.loader.grub.device = "/dev/sda"; # без цифры!
-  boot.loader.timeout = 1;
+  boot.loader.grub.device = "/dev/sda"; # замени sdX на свой диск (например, /dev/sda)
   boot.supportedFilesystems = [ "btrfs" ];
-
-  fileSystems."/" = {
-    device = "/dev/sda2";
-    fsType = "btrfs";
-    options = [ "compress=zstd:2" "ssd" "noatime" "discard=async" "subvol=@" ];
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/sda2";
-    fsType = "btrfs";
-    options = [ "compress=zstd:2" "ssd" "noatime" "discard=async" "subvol=@home" ];
-  };
-
-  fileSystems."/nix" = {
-    device = "/dev/sda2";
-    fsType = "btrfs";
-    options = [ "compress=zstd:2" "ssd" "noatime" "discard=async" "subvol=@nix" ];
-  };
-
-  fileSystems."/var/log" = {
-    device = "/dev/sda2";
-    fsType = "btrfs";
-    options = [ "compress=zstd:2" "ssd" "noatime" "discard=async" "subvol=@log" ];
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/sda1";
-    fsType = "ext4";
-  };
-
-  fileSystems."/swap" = {
-    device = "/dev/sda2";
-    fsType = "btrfs";
-    options = [ "subvol=@swap" ];
-  };
-
-  swapDevices = [{
-    device = "/.swapvol/swapfile";
-  }];
 
   networking.hostName = "nix";
   time.timeZone = "Europe/Moscow";
 
   i18n.defaultLocale = "ru_RU.UTF-8";
-  console.keyMap = "ruwin_cpl";
 
-  services.xserver.enable = true;
-  services.xserver.desktopManager.plasma5.enable = false;
-  services.xserver.windowManager.bspwm.enable = true;
-  services.xserver.displayManager.startx.enable = true;
-  services.xserver.xkb.layout = "us,ru";
-  services.xserver.xkb.options = "grp:alt_shift_toggle";
+  console = {
+    keyMap = "ruwin_cpl";
+    font = "Lat2-Terminus16";
+  };
+
+  services.xserver = {
+    enable = true;
+    layout = "us,ru";
+    xkbOptions = "grp:alt_shift_toggle";
+    displayManager.startx.enable = true;
+    windowManager.bspwm.enable = true;
+  };
+
+  users.users.oleg = {
+    isNormalUser = true;
+    description = "Олег";
+    extraGroups = [ "wheel" "networkmanager" ];
+    password = "tronik"; # ЗАМЕНИ потом через `passwd`
+    shell = pkgs.zsh;
+  };
+
+  programs.zsh.enable = true;
 
   environment.systemPackages = with pkgs; [
     git
-    nvim
+    neovim
     rofi
     polybar
     sxhkd
@@ -76,13 +53,6 @@
     gruvbox
   ];
 
-  users.users.oleg = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    shell = pkgs.zsh;
-  };
-
-  programs.zsh.enable = true;
   services.openssh.enable = true;
 
   system.stateVersion = "24.05";
